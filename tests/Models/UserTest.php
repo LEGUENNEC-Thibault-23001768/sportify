3b3fa97a0b2bcc6a56aa04241086a3fb001b8a55
E.M <m23006367@V-PJ-47-042.salsa.univ-amu.fr>
2024-10-11 11:23:20 +0200

    new test

--- tests/Models/UserTest.php
+++ tests/Models/UserTest.php
@@ -6,11 +6,18 @@ use PHPUnit\Framework\TestCase;
 use Models\User;
 
 class UserTest extends TestCase {
-    public function testFindByEmail() {
+    public function testFindByEmailIsInDb() {
         $user = new User();
         $result = $user->findByEmail('jack@example.com');
         $result_false = $user->findByEmail('CACACACACACACACA@CACACA.COM');
+        
         $this->assertIsArray($result);
+        $this->assertFalse($result_false);
+    }
+
+    public function testFindByEmailIsNotInDb() {
+        $user = new User();
+        $result_false = $user->findByEmail('CACACACACACACACA@CACACA.COM');
         
         $this->assertFalse($result_false);
     }
